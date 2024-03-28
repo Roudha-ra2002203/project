d882545b831b5ff6d6b94dfa6cbae08975c96cbc
Roudha-ra2002203 <157570956+Roudha-ra2002203@users.noreply.github.com>
2024-03-28 19:36:56 +0300

    update datahandler

--- Server/dataHandler.js
+++ Server/dataHandler.js
@@ -5,6 +5,15 @@ const itemsFilePath = './data/item.json';
 const SellersFilePath = './data/Seller.json';
 
 
+const getSellers = () => {
+    const usersData = fs.readFileSync(SellersFilePath);
+
+    return JSON.parse(usersData);
+};
+
+const saveSellers = (Sellers) => {
+    fs.writeFileSync(SellersFilePath, JSON.stringify(Sellers, null, 2));
+};
 
 const getUsers = () => {
     const usersData = fs.readFileSync(usersFilePath);
@@ -12,9 +21,19 @@ const getUsers = () => {
     return JSON.parse(usersData);
 };
 
+const getItems = () => {
+    const itemsData = fs.readFileSync(itemsFilePath);
+    return JSON.parse(itemsData);
+};
+
 const saveUsers = (users) => {
     fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
 };
+
+const saveItems = (items) => {
+    fs.writeFileSync(itemsFilePath, JSON.stringify(items, null, 2));
+};
+
 
 //
 module.exports = {
