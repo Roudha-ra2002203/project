323b1073b13c12d9d12dce51fccfd2987425243a
Roudha-ra2002203 <157570956+Roudha-ra2002203@users.noreply.github.com>
2024-03-28 19:11:30 +0300

    update api

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -21,7 +21,19 @@ router.post('/login', (req, res) => {
 });
 
 // Items Endpoints
-//
+router.get('/items', (req, res) =>{
+    const items = getItems();
+     res.json(items);
+});
+
+router.post('/items', (req, res) => {
+    const newItem = req.body;
+    const items = getItems();
+    newItem.id = (items.length + 1).toString(); // Assign a new unique ID
+    items.push(newItem);
+    saveItems(items);
+    res.status(201).json(newItem);
+});
 
 
 module.exports = router;
